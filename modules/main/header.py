0a93ce9262299b9aaad302344c5ddb8abeb25387
laskovy <laskovyjsvatoslav@gmail.com>
2026-04-19 10:53:01 +0300

    add buttons

--- modules/main/header.py
+++ modules/main/header.py
@@ -1,6 +1,54 @@
-from PyQt6.QtWidgets import QFrame
+from PyQt6.QtWidgets import QFrame, QLabel, QPushButton, QLineEdit, QHBoxLayout
+
 
 class Header(QFrame):
     def __init__(self):
         QFrame.__init__(self)
+
         self.setFixedSize(790, 36)
+
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(10, 0, 10, 0)
+        self.layout.setSpacing(10)
+
+        
+        self.settings_btn = QPushButton("⚙️")
+        self.settings_btn.setFixedSize(30, 30)
+        self.settings_btn.setStyleSheet("background-color: rgba(255,255,255,0.1); border-radius: 6px; color: white;")
+
+        self.title = QLabel("Налаштування")
+        self.title.setStyleSheet("color: white;")
+
+        self.layout.addWidget(self.settings_btn)
+        self.layout.addWidget(self.title)
+
+        
+        self.layout.addStretch()
+
+        
+        self.add_btn = QPushButton("Додати")
+        self.add_btn.setFixedHeight(28)
+        self.add_btn.setStyleSheet("""
+            background-color: rgba(255,255,255,0.1);
+            border-radius: 8px;
+            color: white;
+            padding: 0 10px;
+        """)
+
+        
+        self.input = QLineEdit()
+        self.input.setPlaceholderText("...")
+        self.input.setFixedSize(180, 28)  
+
+        self.input.setStyleSheet("""
+            background-color: rgba(255,255,255,0.1);
+            border-radius: 8px;
+            padding-left: 8px;
+            color: white;
+        """)
+
+        
+        self.layout.addWidget(self.add_btn)
+        self.layout.addWidget(self.input)
+
+        self.setLayout(self.layout)
